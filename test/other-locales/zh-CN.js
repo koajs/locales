26871342a079461aa5237536bc98a28da8d255bf
Jason Lee <huacnlee@gmail.com>
2015-09-18 16:04:22 +0800

    fix: multiple locales merge in nested case.
    - Removed merge-merge-descriptors npm;
    - Add test case with nested merge.

--- test/other-locales/zh-CN.js
+++ test/other-locales/zh-CN.js
@@ -1,3 +1,14 @@
 module.exports = {
-  Email: '邮箱1'
-}
+  Email: '邮箱1',
+  model: {
+    user: {
+      fields: {
+        name: "姓名",
+        email: '邮箱'
+      },
+      hints: {
+        name: '这里填写你的真名'
+      }
+    }
+  }
+};
